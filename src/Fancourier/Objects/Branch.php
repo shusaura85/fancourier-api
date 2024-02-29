9a4d5d3e5a7e8ffada2b843f3f9e26054132e282
Dan-Radu Pana <danradu.pana@gmail.com>
2024-02-29 23:25:46 +0200

    [fix] Class properties typos

--- src/Fancourier/Objects/Branch.php
+++ src/Fancourier/Objects/Branch.php
@@ -17,15 +17,15 @@ class Branch
 	protected $addr_countyId;
 	protected $addr_cityId;
 	protected $addr_street;
-	protected $addr_streeNo;
+	protected $addr_streetNo;
 	protected $addr_zipcode;
 	protected $addr_building;
 	protected $addr_entrance;
 	protected $addr_floor;
 	protected $addr_apartment;
-	
+
 	public function __construct($data)
-		{
+	{
 		$this->id = $data['id'];
 		$this->name = $data['name'];
 		$this->bank = $data['bank'];
@@ -45,101 +45,100 @@ class Branch
 		$this->addr_entrance = $data['address']['entrance'];
 		$this->addr_floor = $data['address']['floor'];
 		$this->addr_apartment = $data['address']['apartment'];
-		}
-	
+	}
+
 	public function getId(): string
-		{
+	{
 		return $this->id;
-		}
-	
+	}
+
 	public function getName(): string
-		{
+	{
 		return $this->name;
-		}
-	
+	}
+
 	public function getBank(): string
-		{
+	{
 		return $this->bank;
-		}
-	
+	}
+
 	public function getBankAccount(): string
-		{
+	{
 		return $this->bankAccount;
-		}
-	
+	}
+
 	public function getEmail(): string
-		{
+	{
 		return $this->email;
-		}
-	
+	}
+
 	public function getPhone(): string
-		{
+	{
 		return $this->phone;
-		}
-	
+	}
+
 	public function getSecondaryPhone(): string
-		{
+	{
 		return $this->altPhone;
-		}
-	
+	}
+
 	public function getContactPerson(): string
-		{
+	{
 		return $this->contactPerson;
-		}
-	
+	}
+
 	public function getCounty(): string
-		{
-		return $this->county;
-		}
-	
+	{
+		return $this->addr_county;
+	}
+
 	public function getCity(): string
-		{
+	{
 		return $this->addr_city;
-		}
-	
+	}
+
 	public function getCountyId(): string
-		{
-		return $this->countyId;
-		}
-	
+	{
+		return $this->addr_countyId;
+	}
+
 	public function getCityId(): string
-		{
+	{
 		return $this->addr_cityId;
-		}
-	
+	}
+
 	public function getStreet(): string
-		{
+	{
 		return $this->addr_street;
-		}
-	
+	}
+
 	public function getStreetNo(): string
-		{
+	{
 		return $this->addr_streetNo;
-		}
-	
+	}
+
 	public function getPostalCode(): string
-		{
+	{
 		return $this->addr_zipcode;
-		}
-	
+	}
+
 	public function getBuilding(): string
-		{
+	{
 		return $this->addr_building;
-		}
-	
+	}
+
 	public function getEntrance(): string
-		{
+	{
 		return $this->addr_entrance;
-		}
-	
+	}
+
 	public function getFloor(): string
-		{
+	{
 		return $this->addr_floor;
-		}
-	
+	}
+
 	public function getApartment(): string
-		{
+	{
 		return $this->addr_apartment;
-		}
-	
+	}
 }
