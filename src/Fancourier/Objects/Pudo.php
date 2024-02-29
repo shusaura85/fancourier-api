d591d7b9abda6ddb0605a133f2c6b200bc0de6a6
Dan-Radu Pana <danradu.pana@gmail.com>
2024-02-29 23:04:52 +0200

    [fix] Pudo Object id intval conversion dropped

--- src/Fancourier/Objects/Pudo.php
+++ src/Fancourier/Objects/Pudo.php
@@ -10,95 +10,93 @@ class Pudo
 	protected $description;
 	protected $latitude;
 	protected $longitude;
-	
+
 	protected $address;
-	
+
 	protected $schedule;
 	protected $drawer;
-	
+
 	protected $phones;	// array
 	protected $email;
-	
+
 	public function __construct($data)
-		{
-		$this->id				= intval($data['id'] ?? 0);
+	{
+		$this->id				= $data['id'] ?? 0;
 		$this->name				= $data['name'] ?? '';
 		$this->routingLocation	= $data['routingLocation'] ?? '';
 		$this->description		= $data['description'] ?? '';
-		
+
 		$this->address			= $data['address'] ?? [];
-		
+
 		$this->latitude			= $data['latitude'] ?? '';
 		$this->longitude		= $data['longitude'] ?? '';
-		
+
 		$this->schedule			= $data['schedule'] ?? [];
 		$this->drawer			= $data['drawer'] ?? [];
-		
+
 		$this->phones			= $data['phones'] ?? [];
-		
+
 		$this->email			= $data['email'] ?? '';
-		}
-	
+	}
+
 	public function getId(): string
-		{
+	{
 		return $this->id;
-		}
-	
+	}
+
 	public function getName(): string
-		{
+	{
 		return $this->name ?? '';
-		}
-		
+	}
+
 	public function getRoutingLocation(): string
-		{
+	{
 		return $this->routingLocation ?? '';
-		}
-	
+	}
+
 	public function getDescription(): string
-		{
+	{
 		return $this->description ?? '';
-		}
-	
+	}
+
 	public function getLatitude(): string
-		{
+	{
 		return $this->latitude ?? '';
-		}
-	
+	}
+
 	public function getLongitude(): string
-		{
+	{
 		return $this->longitude ?? '';
-		}
-	
+	}
+
 	public function getAddress(): array
-		{
+	{
 		return $this->address ?? '';
-		}
-	
+	}
+
 	public function getSchedule() //: array|string
-		{
+	{
 		return $this->schedule ?? '';
-		}
-	
+	}
+
 	public function getDrawer() //: array|string
-		{
+	{
 		return $this->drawer ?? '';
-		}
-	
+	}
+
 	public function getPhones() //: array|string
-		{
+	{
 		return $this->phones ?? '';
-		}
-	
+	}
+
 	public function getEmail(): string
-		{
+	{
 		return $this->email ?? '';
-		}
-	
-	
-	
+	}
+
 	/* return array with data similar to fan courier api response */
 	public function getArray(): array
-		{
+	{
 		$arr = [
 			"id"				=> $this->id,
 			"name"				=> $this->name,
@@ -111,9 +109,8 @@ class Pudo
 			"drawer"			=> $this->drawer,
 			"phones"			=> $this->phones,
 			"email"				=> $this->email,
-			];
-		
+		];
+
 		return $arr;
-		}
-	
+	}
 }
