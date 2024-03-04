10554eff7570b6ed19e0f7e8f1af92e21f624f5b
Shu Saura <35106430+shusaura85@users.noreply.github.com>
2024-03-04 12:56:37 +0200

    Add ZPL and Size options
    You can now request AWB's in ZPL format
    You can now specify AWB's size

--- src/Fancourier/Request/PrintAwb.php
+++ src/Fancourier/Request/PrintAwb.php
@@ -9,16 +9,34 @@ class PrintAwb extends AbstractRequest implements RequestInterface
 
     private $awbs = [];
     private $pdf = true;
+    private $zpl = false;
     private $lang = 'ro';
+    private $size = '';
 
     public function pack()
     {
         $arr = [
 			'clientId'	=> $this->auth->getClientId(),
             'awbs' => $this->awbs,
-            'pdf' => ($this->pdf ? 1 : 0),
-            'language' => $this->lang,
+            'language' => $this->lang
         ];
+		
+		// send pdf variable only if active (can't send both pdf and zpl at the same time)
+		if ($this->pdf)
+			{
+			$arr['pdf'] = 1;
+			}
+		else // send zpl variable only if active
+		if ($this->zpl)
+			{
+			$arr['zpl'] = 1;
+			}
+		
+		// add the format only if user requests a specific size
+		if ($this->size != '')
+			{
+			$arr['format'] = $this->size;
+			}
 		
 		return $arr;
     }
@@ -65,6 +83,26 @@ class PrintAwb extends AbstractRequest implements RequestInterface
     public function setPdf($active = true)
     {
         $this->pdf = $active;
+        if ($this->zpl) { $this->zpl = false; }	// disable ZPL in case it's active
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZpl()
+    {
+        return $this->zpl;
+    }
+
+    /**
+     * @param bool $active
+     * @return PrintAwb
+     */
+    public function setZpl($active = false)
+    {
+        $this->zpl = $active;
+        if ($this->pdf) { $this->pdf = false; }	// disable PDF in case it's active
         return $this;
     }
 
@@ -88,6 +126,29 @@ class PrintAwb extends AbstractRequest implements RequestInterface
         }
 
         $this->lang = $lang;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSize()
+    {
+        return $this->lang;
+    }
+
+    /**
+     * @param string $pageSize - Can be <empty>, 'A4', 'A5' and 'A6' (only for ePOD)
+     * @return PrintAwb
+     */
+    public function setSize($pageSize = '')
+    {
+        $pageSize = strtoupper($pageSize);
+        if (!in_array($pageSize, ['A4', 'A5', 'A6'])) {
+            $pageSize = '';
+        }
+
+        $this->size = $pageSize;
         return $this;
     }
 }
